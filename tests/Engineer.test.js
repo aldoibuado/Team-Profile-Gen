47af90d1fb7b041fa6e6088fc51462833b4987b0
aldoibuado <aldo.ibuado@gmail.com>
2022-11-27 19:54:34 -0800

    added github username

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,22 +1,27 @@
-const Engineer = require('../lib/Engineer');
+const Engineer = require("../lib/Engineer");
 
-test('github', () => {
-    const github = 'aldoibuado'
-    const employee = new Engineer('Aldo', 30, 'aldo.ibuado@gmail.com', github)
+test("github", () => {
+  const github = "aldoibuado";
+  const employee = new Engineer("Aldo", 30, "aldo.ibuado@gmail.com", github);
 
-    expect(employee.github).toBe(github);
+  expect(employee.github).toBe(github);
 });
 
-test('testing to see if getGithub method works', () => {
-    const github1 = 'aldoibuado'
-    const employee = new Engineer('Aldo', 30, 'aldo.ibuado@gmail.com', github1)
+test("testing to see if getGithub method works", () => {
+  const github1 = "aldoibuado";
+  const employee = new Engineer("Aldo", 30, "aldo.ibuado@gmail.com", github1);
 
-    expect(employee.getGithub()).toBe(github1);
+  expect(employee.getGithub()).toBe(github1);
 });
 
-test('testing to see if getRole method works', () => {
-    const role = 'Engineer'
-    const employee = new Engineer('Aldo', 30, 'aldo.ibuado@gmail.com', 'aldoibuado')
+test("testing to see if getRole method works", () => {
+  const role = "Engineer";
+  const employee = new Engineer(
+    "Aldo",
+    30,
+    "aldo.ibuado@gmail.com",
+    "aldoibuado"
+  );
 
-    expect(employee.getRole()).toBe(role);
+  expect(employee.getRole()).toBe(role);
 });

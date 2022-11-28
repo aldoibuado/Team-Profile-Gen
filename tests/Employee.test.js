985cf61a6fddcfe2fc608901bc782500b4de00aa
aldoibuado <aldo.ibuado@gmail.com>
2022-11-27 19:49:53 -0800

    tested objects for engineer role

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -26,3 +26,32 @@ test('email', () => {
 
     expect(employee.email).toBe(email);
 })
+
+test('testing if getName method works', () => {
+    const name1 = 'Aldo'
+    const employee = new Employee(name1)
+
+    expect(employee.getName()).toBe(name1);
+})
+
+test('testing if getId method works', () => {
+    const id1 = 30
+    const employee = new Employee('Aldo',id1)
+
+    expect(employee.getId()).toBe(id1);
+})
+
+test('testing if getEmail method works', () => {
+    const email1 = 'aldo.ibuado@gmail.com'
+    const employee = new Employee('Aldo', 30, email1)
+
+    expect(employee.getEmail()).toBe(email1);
+})
+
+test('testing if getRole method works', () => {
+    const role = 'Employee'
+    const employee = new Employee('Aldo', 30, 'aldo.ibuado@gmail.com')
+
+    expect(employee.getRole()).toBe(role);
+})
+
